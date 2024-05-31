7356e59caa574b8cd07d83186ef36f5066c4153f
Erin Hamrick <erin.hamrick76@gmail.com>
2024-05-31 16:11:30 -0500

    added functionality to clear list items when x icon is clicked

--- script.js
+++ script.js
@@ -3,40 +3,46 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
 function addItem(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    const newItem = itemInput.value;
-    // validate input
-    if (newItem === '') {
-        alert('Please add an item');
-        return;
-    }
-    // create list item
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(newItem));
-    
-    const button = createButton('remove-item btn-link text-red');
-    li.appendChild(button);
-    
-    itemList.appendChild(li);
+	const newItem = itemInput.value;
+	// validate input
+	if (newItem === '') {
+		alert('Please add an item');
+		return;
+	}
+	// create list item
+	const li = document.createElement('li');
+	li.appendChild(document.createTextNode(newItem));
 
-    itemInput.value = '';
+	const button = createButton('remove-item btn-link text-red');
+	li.appendChild(button);
+
+	itemList.appendChild(li);
+
+	itemInput.value = '';
 }
 
 function createButton(classes) {
-    const button = document.createElement('button');
-    button.className = classes;
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
-    return button;
+	const button = document.createElement('button');
+	button.className = classes;
+	const icon = createIcon('fa-solid fa-xmark');
+	button.appendChild(icon);
+	return button;
 }
 
 function createIcon(classes) {
-    const icon = document.createElement('i');
-    icon.className = classes;
-    return icon;
+	const icon = document.createElement('i');
+	icon.className = classes;
+	return icon;
+}
+function removeItem(e) {
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		const li = e.target.parentElement.parentElement;
+		li.remove();
+	}
 }
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem)
-
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
